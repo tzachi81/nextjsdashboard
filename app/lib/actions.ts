eb9c3bf7cf36756a6d78e12fad02128b8d7067c7
tzachi81 <tzahih@gmail.com>
2024-12-02 12:40:27 +0200

    error handling with error.tsx

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -78,7 +78,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 export async function deleteInvoice(id: string) {
   
   //DELETE this dmi error throw
-  // throw new Error('Failed to Delete Invoice');
+  throw new Error('Failed to Delete Invoice');
   //******************************/
 
   try {
