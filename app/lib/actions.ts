504f8674457daec3c449788370b4840be0acf7b4
tzachi81 <tzahih@gmail.com>
2024-12-03 11:15:17 +0200

    Authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,10 @@
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
+import { redirect } from 'next/navigation'; 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 
 export type TState = {
   errors?: {
@@ -121,5 +124,24 @@ export async function deleteInvoice(id: string) {
 
   } catch (error) {
     return { message: 'Database Error: "Delete Invoice" action failed.', error }
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
